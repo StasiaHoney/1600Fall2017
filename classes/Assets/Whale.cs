09cee7d4e8a19db9a893b0a6fa354c5928a167ef
Unknown <kosukeinuzuka@gmail.com>
2017-08-30 11:38:50 -0600

    Unity

---
+++ classes/Assets/Whale.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whale : Mammal {
+
+public override void Start(){
+		base.Start();
+		Swim();
+	}
+	void Swim(){
+		print(this.name + " Swims");
+	}
+}
