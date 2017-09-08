ec58b00d4246d32ed7ddbce30603e2faa490e31d
Ana Chumakov <kosukeinuzuka@gmail.com>
2017-09-08 11:48:41 -0600

    Vars in class

---
+++ 1600Fall2017 stuff/Assets/Vars.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vars : MonoBehaviour {
+
+	public float health = 100;
+
+	void Start () {
+		// health = 200;
+	}
+	
+	void OnTriggerEnter()
+	{
+		health += 20;
+	}
+
+	void Update () {
+		// if (health > 0)
+		// {
+		// 	health -= 0.1f;
+		// } else
+		// {
+		// 	print("You Died!");
+		// }
+	}
+}
