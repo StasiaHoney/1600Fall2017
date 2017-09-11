9217caf4a2370ded750c40a805450a3b4f4215e4
Ana Chumakov <kosukeinuzuka@gmail.com>
2017-09-11 11:51:50 -0600

    9/11/17 if statements

---
+++ 1600Fall2017 stuff/Assets/ifStatements.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ifStatements : MonoBehaviour {
+
+	public Text input;
+	private string password = "G@m3rG33k";
+	
+	void Update ()
+	{
+		if (input.text == password)
+		{
+			print("You know the password");
+		}
+	}
+}
