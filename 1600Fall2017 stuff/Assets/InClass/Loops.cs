76c525499d9d2445931b7045d987a797681bd9b8
Ana Chumakov <kosukeinuzuka@gmail.com>
2017-09-20 15:38:26 -0600

    While Loops assignment

--- 1600Fall2017 stuff/Assets/InClass/Loops.cs
+++ 1600Fall2017 stuff/Assets/InClass/Loops.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class Loops : MonoBehaviour {
 
 	public bool canPlay = true;
-
+	public bool health = true;
+	public bool stamina = true;
+	public bool shield = false;
+	public bool wound = true;
+	public bool healthPickup = false;
+	public float healing = 200;
 	void Start()
 	{
 		StartCoroutine(PlayGame());
+		StartCoroutine(Health());
+		StartCoroutine(Stamina());
+		StartCoroutine(Shield());
+		StartCoroutine(Wound());
+		StartCoroutine(healthpickup());
 	}
 
 	IEnumerator PlayGame(){
@@ -17,6 +27,47 @@ public class Loops : MonoBehaviour {
 			print("Playing");
 			yield return new WaitForSeconds(0.1f);
 		}
-		print("End Game");
+		print("End Game");//while playing print playing continuously, after playing print end game once
+	}
+
+	IEnumerator Health(){
+		while (health)
+		{
+			print("Healing");
+			yield return new WaitForSeconds(0.1f);
+		}
+		print("Healing Pickup Expired");
+	}
+
+	IEnumerator Stamina(){
+		while (stamina){
+			print ("Gaining Stamina");
+			yield return new WaitForSeconds(0.1f);
+		}
+		print("Stamina Pickup Expired");
+	}
+
+	IEnumerator Shield(){
+		while (shield == true){
+			print("Shield Up"); 
+			yield return new WaitForSeconds(0.1f);
+		}
+		print("Shield Down");
+	}
+
+	IEnumerator Wound(){
+		while (wound){
+			print("Wounded");
+			yield return new WaitForSeconds(0.1f);
+		}
+		print("Healed");
+	}
+
+	IEnumerator healthpickup(){
+		while (healthPickup == true){
+			healing += 20;
+			yield return new WaitForSeconds(0.1f);
+		}
+		print("Pickup Expired");
 	}
 }
