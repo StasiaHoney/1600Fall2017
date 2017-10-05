2f620f1da2ec2d3ae36a0bb28c24f7c4d9a559f8
Ana Chumakov <kosukeinuzuka@gmail.com>
2017-10-04 21:39:43 -0600

    For Loops Assignment

--- 1600Fall2017 stuff/Assets/Assignments/ForLoopsAssignment.cs
+++ 1600Fall2017 stuff/Assets/Assignments/ForLoopsAssignment.cs
@@ -8,26 +8,58 @@ public class ForLoopsAssignment : MonoBehaviour {
 		var array = new []{1,2,3,4,5};
 		int sum = 0;
 
-		for ( int i=0; i<array.Length; i++ )
+		for ( int i=0; i<array.Length; i++ ) //takes a list or array and some perameters you set to output stuff...
 		{
-			print(string.Format("array [{0}]={1}", i, array[i]));
-			sum += array[i];
+			print(string.Format("array [{0}]={1}", i, array[i])); // this prints every array element as well as it's element #
+			sum += array[i]; //adds all elements together
 		}
 
-		print(string.Format("sum of all elements in the array is {0}", sum));
+		print(string.Format("sum of all elements in the array is {0}", sum)); //prints sum of elements
 		
 		var myAge = new []{18,19,20,21,22,23};
-		int age = 18;
 		
-		for( int i = 18; i<myAge.Length; i+= 4)
+		for( int age = 18; age<myAge.Length; age += 4)
 		{
 			print(string.Format("My age is {0}", age));
 		}
 		
 		int x=1;
-		for( int i=0; i<10; i++)
+		for( int i=0; i<10; i++)//can also use ints in for loops
 		{
 			x = x*(x+1);
+			print(string.Format("value of x is {0}", x));
+		}
+
+		for (float f = 1.0f ; f < 2.0f; f += 0.3f)
+		{
+			print(string.Format("Float number is {0}", f));
+		}
+
+		float sumf = 5.6f;
+		for( float f = 5.6f ; f>= 4.2; f -= 0.1f){
+			print(string.Format("Float Subtraction is {0}", f));
+			sumf += f;
+		}
+		print(string.Format("sum of float f is {0}", sumf));
+
+		for(int i = 4; i <= 0; i-= 1 ){
+			print(string.Format("int subtraction is {0}", i));
+		}
+
+		for(float f = 110.7f; f < 120.5; f++){
+			print(string.Format("float is {0}", f));
+		}
+
+		for(double d = 1.1D; d < 1.4; d += .001D){
+			print(string.Format("double d is {0}", d));
+		}
+
+		for(float f = .1f; f>.001; f*=.1f){
+			print(string.Format("float mult is {0}", f));
+		}
+
+		for(double d = .1D ; d<100D; d/= .1D ){
+			print(string.Format("double div is {0}", d));
 		}
 	}
 	
