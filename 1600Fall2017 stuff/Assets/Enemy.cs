5ccb594b20cbbfd8faf9ea535947571c49c32c1b
Ana Chumakov <kosukeinuzuka@gmail.com>
2017-10-11 11:56:50 -0600

    AI chase

---
+++ 1600Fall2017 stuff/Assets/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+	public NavMeshAgent Agent;
+	public Transform player;
+	
+	// Update is called once per frame
+	void Update () {
+		Agent.destination = player.position;
+	}
+
+	public void Stun (float _speed) {
+		Agent.speed = _speed;
+	}
+}
