f5d7bc87bbf3ba38ed207acb14aa6b31a4e4dc29
Ana Chumakov <kosukeinuzuka@gmail.com>
2017-10-13 11:57:15 -0600

    checkpoint

---
+++ 1600Fall2017 stuff/Assets/SaveCheckpoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveCheckpoint : MonoBehaviour {
+
+	Vector3 checkPoint;
+	public Transform Player;
+	string [] playerPrefsTitles = {"CheckpointX", "CheckpointY", "CheckpointZ"};
+	void OnTriggerEnter()
+	{
+		checkPoint = transform.position;
+		for (int i = 0; i < playerPrefsTitles.Length; i++)
+		{
+			PlayerPrefs.SetFloat(playerPrefsTitles[i], checkPoint[i]);
+		}			
+	}
+
+	void Start()
+	{
+		Player.position = checkPoint;
+		for (int i = 0; i < playerPrefsTitles.Length; i++)
+		{
+			checkPoint[i] = PlayerPrefs.GetFloat(playerPrefsTitles[i]);
+		}		
+	}
+}
