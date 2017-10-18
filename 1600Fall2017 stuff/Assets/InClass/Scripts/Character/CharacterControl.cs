da307120031392343d03a6ccba754cafa8601491
Ana Chumakov <kosukeinuzuka@gmail.com>
2017-10-18 11:47:16 -0600

    added ai on camera

--- 1600Fall2017 stuff/Assets/InClass/Scripts/Character/CharacterControl.cs
+++ 1600Fall2017 stuff/Assets/InClass/Scripts/Character/CharacterControl.cs
@@ -3,27 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour {
-
-	public CharacterController characterController;
+public CharacterController characterController;
 	public float gravity = 9.81f;
+	public float speed = 10;
 	public Vector3 moveVector3;
-	public float speed = 12;
-	public float jumpForce = 100; 
-
-	void Start () {
-		
-	}
-	
-	
+	public float jumpForce = 20;
 	void FixedUpdate () {
 		moveVector3.y -= gravity * Time.deltaTime;
-		if(characterController.isGrounded)
+		if (characterController.isGrounded)
 		{
-			if(Input.GetKey(KeyCode.Space))
+			if (Input.GetKey(KeyCode.Space))
 			{
 				moveVector3.y += jumpForce * Time.deltaTime;
 			}
-			
 			moveVector3.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 		}
 		characterController.Move(moveVector3);
