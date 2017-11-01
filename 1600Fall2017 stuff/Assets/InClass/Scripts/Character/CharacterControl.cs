c37a21ecc175fa6bab412f0c843613a9d4559b03
Ana Chumakov <kosukeinuzuka@gmail.com>
2017-11-01 11:49:18 -0600

    fixed jump and started new scene

--- 1600Fall2017 stuff/Assets/InClass/Scripts/Character/CharacterControl.cs
+++ 1600Fall2017 stuff/Assets/InClass/Scripts/Character/CharacterControl.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour {
-public CharacterController characterController;
+
+	public CharacterController characterController;
 	public static bool gameOver = false;
 	public float gravity = 9.81f;
 	public float speed = 10;
 	public Vector3 moveVector3;
 	public float jumpForce = 20;
-	void FixedUpdate () {
+	void Update () {
 		moveVector3.y -= gravity * Time.deltaTime;
 		if (characterController.isGrounded && !gameOver)
 		{
-			if (Input.GetKey(KeyCode.Space))
+			if (Input.GetKeyDown(KeyCode.UpArrow))
 			{
-				moveVector3.y += jumpForce * Time.deltaTime;
+				moveVector3.y = jumpForce * Time.deltaTime;
 			}
 			moveVector3.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 		}
