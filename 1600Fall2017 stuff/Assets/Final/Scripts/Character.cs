066398df6881be6518777e5b62e915a53d107bef
Ana Chumakov <kosukeinuzuka@gmail.com>
2017-11-09 19:59:25 -0700

    worked on final, added charactercontroller on player

---
+++ 1600Fall2017 stuff/Assets/Final/Scripts/Character.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	public CharacterController characterControl;
+	public static bool gameover = false;
+	public float gravity = 9.81f;
+	public float speed = 15;
+	public float jump = 50;
+	public Vector3 moveVector3;
+
+	void Update()
+	{
+		moveVector3.y -= gravity * Time.deltaTime;
+
+		if(characterControl.isGrounded && !gameover)
+		{
+			if(Input.GetKeyDown(KeyCode.UpArrow))
+			{
+				moveVector3.y = jump * Time.deltaTime;
+			}
+			moveVector3.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		}
+		characterControl.Move(moveVector3);
+	}
+}
